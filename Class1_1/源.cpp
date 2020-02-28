5f9cbf394769f79ddf0b82e4468a87468c51502b
天昱 郑 <1219979760@qq.com>
2020-02-28 14:34:10 +0800

    添加项目文件。

---
+++ Class1_1/源.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cv::Mat srcMat = imread("D:\\a.jpg");
+	int height = srcMat.rows;
+	int width = srcMat.cols;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+	}
+	imshow("src",srcMat);
+	waitKey(0);
+	return 0;
+
+}
